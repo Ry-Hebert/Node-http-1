6c4379a8dd4ddade6df5f050d31ffdd4a89b8a4f
Ryan <ryan@thecpafirm.com>
2021-03-22 23:27:31 -0600

    stuck in trafic wreck update

--- server.js
+++ server.js
@@ -1,15 +1,82 @@
-import Express from 'express'
-import { serverRouter } from './serverRouter'
+require('dotenv').config()
 
-const server = Express()
-const port = process.env.PORT || 5000
+const Express = require('express')
+const Mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const LikedShips = require('./models/likedShips')
 
-server.use(Express.static('public'))
+const server = new Express()
 
-server.use((req, res, next) => {
-    res.status(404).send('<h1>Error: 404</h1><br><h2>Page not found</h2>')
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+Mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+
+server.listen(process.env.PORT || 3001, () =>{
+    console.log('Server is running')
 })
 
-server.use('/api', serverRouter)
+server.use('/', Express.static('./src'))
 
-server.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
+server.get('/model/likedShips', (req, res) => {
+    LikedShips.find({}, (err, items) =>{
+
+        if(err){console.log(handleError(err))}
+        res.json(items)
+    })
+})
+
+server.post('/model/likedShips', (req, res) => {
+    console.log(req.query)
+    
+    let qData = []
+
+    LikedShips.find({}, (err, data) =>{
+        if(err){console.log(err)}
+        else{
+            console.log(data)
+            qData.push(data)
+        }
+    })
+
+    console.log(`Find Test: ${qData}`)
+
+    let catSize = qData.filter(item =>{
+        return item.category === req.query.category
+    })
+    
+    let catLength = catSize.length
+
+    LikedShips.create({
+    todo: req.query.todo,
+    complete: req.query.complete,
+    category: req.query.category,
+    categoryID: catLength + 1
+    })
+
+    res.sendStatus(200)
+})
+
+server.put('/model/likedShips/:id', (req, res) =>{
+    LikedShips.findById(req.params.id, (err, items) =>{
+        if(err){console.log(handleError(err))}
+        items.update(req.query, (err) =>{
+            if(err){console.log(handleError(err))}
+            LikedShips.find({}, (err, itemsX) =>{
+                if(err){console.log(handleError(err))}
+                res.json(itemsX)
+            })
+        })
+    })
+})
+
+server.delete('/model/likedShips/:id', (req, res) =>{
+    console.log(`This is the delete route: ${req.params.id}`)
+    LikedShips.remove({id: req.params.id}, (err) => {
+        if(err){console.log(handleError(err))}
+        LikedShips.find({}, (err, items) =>{
+            if(err){console.log(handleError(err))}
+            res.json(items)
+        })
+    })
+})
