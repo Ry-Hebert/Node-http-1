283ee0bbd84e74821c12d57a236a84109c6a00c6
Ryan <ryan@thecpafirm.com>
2021-03-26 10:30:39 -0600

    cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(bodyParser.json());
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS")
+    res.header.Access-Control-Allow-Methods( POST, GET, PUT, DELETE, OPTIONS)
     next();
   })
 
